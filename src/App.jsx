e57d48e7506adb723d41e7d41ecbb4abb611a108
shamssaniat <shams.saniat@northsouth.edu>
2025-07-03 16:43:39 +0600

    Developed Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="pattern" />
+
+      <div className="wrapper" />
+      <header>
+        <img src="hero.png" alt="" />
+        <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+      </header>
+
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}></Search>
+      <h1 className='text-white'>{searchTerm}</h1>
     </main>
   )
 }
